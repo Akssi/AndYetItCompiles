cc6043f5735c211c13978d0ed3d8f892f286f6ad
Théophane Frémond-Guilbault <theophane.fremond-guilbault@polytmtl.ca>
2018-01-20 15:25:42 -0500

    on pull maintenant les decisions du fake server

--- app/mock_server/server.js
+++ app/mock_server/server.js
@@ -16,6 +16,32 @@ app.get('/categories', function(req, res) {
   res.send(JSON.stringify(response));
 })
 
+app.get('/decisions', function(req, res) {
+  let categories1 = ["transport", "societe"]; 
+  let decision1 = {
+    id :"id1",
+    title : "ceci est une premiere decision",
+    content : "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla",
+    categories : categories1 
+  };
+  let categories2 = ["transport", "societe"]; 
+  let decision2 = {
+    id :"id1",
+    title : "ceci est une premiere decision",
+    content : "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla",
+    categories : categories2 
+  };
+  let categories3 = ["transport", "societe"]; 
+  let decision3 = {
+    id :"id1",
+    title : "ceci est une premiere decision",
+    content : "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla",
+    categories : categories3 
+  };
+  let response = [decision1, decision2, decision3];
+  res.send(JSON.stringify(response));
+})
+
 var server = app.listen(8081, function () {
   var host = server.address().address;
   var port = server.address().port;
